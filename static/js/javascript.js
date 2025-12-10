5ad6db5774bae8b5139cf9ad3badfd78ba68812b
lharvey77 <lilibetharvey@outlook.com>
2025-12-10 12:56:46 +0100

    Navigation and Files

---
+++ static/js/javascript.js
@@ -0,0 +1,8 @@
+function hamburgerMenu() {
+  var menu = document.getElementById("hamburger-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+} 
